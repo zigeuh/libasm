0c672c4fa0917ae576843574695725b481e1271d
zigeuh <zigeuh13@gmail.com>
2025-11-28 10:33:34 +0100

    Finished ft_list_sort + tests

--- bonus/ft_list_sort.s
+++ bonus/ft_list_sort.s
@@ -2,47 +2,68 @@ section .text
     global ft_list_sort
 
 ft_list_sort:
+    push rbp
+    mov rbp, rsp
+
+    push rdi
+    push rsi
+
     test rdi, rdi
     jz .done
 
-    mov rdx, [rdi]
+    mov rdx, [rdi]          ; rdx = current
+
     test rdx, rdx
     jz .done
 
     test rsi, rsi
     jz .done
 
-    mov rbx, rsi ; rbx = cmp function
-    mov rax, 1 ; swapped = 1
+    mov r8, rsi
+    mov eax, 1
 
 .outer_loop:
-    cmp rax, 0
-    je .done
+    test eax, eax
+    jz .done
 
-    mov rax, 0     ; swapped = 0
-    mov rdx, [rdi] ; rdx = current
+    mov rcx, [rdx + 8]      ; rdx = current->next
+    mov eax, 0
 
 .inner_loop:
-    mov r8, [rdx + 8] ; r8 = current->next
-    test r8, r8
-    jz .outer_loop
+    test rcx, rcx
+    jz .end_inner_loop
 
     mov rdi, [rdx]
-    mov rsi, [r8]
-    call rbx
-    cmp rax, 0 ; swapped = 0
-    jne .no_swap
+    mov rsi, [rcx]
+    push rdx
+    push rcx
+    push r8
+    sub rsp, 8
+    call r8
+    add rsp, 8
+    pop r8
+    pop rcx
+    pop rdx
 
-    mov r9, [rdx]         ; tmp = current->data
-    mov r10, [r8]          ; tmp2 = current->next->data
-    mov [rdx], r10
-    mov [r8], r9
-    
-    mov rax, 1
+    test eax, eax
+    jz .not_swap
 
-.no_swap:
-    mov rdx, r8            ; current = current->next
+    mov r9, [rdx]           ; tmp = current->data
+    mov r10, [rcx]          ; tmp2 = current->next->data
+    mov [rdx], r10          ; current->data = tmp2
+    mov [rcx], r9           ; current->next->data = tmp
+    mov eax, 1
+
+.not_swap:
+    mov rcx, [rcx + 8]      ; current->next = current->next->next
     jmp .inner_loop
 
+.end_inner_loop:
+    mov rdx, [rdx + 8]      ; current = current->next
+    jmp .outer_loop
+
 .done:
+    pop rsi
+    pop rdi
+    pop rbp
     ret
--- bonus/tests/ft_list_sort_tests.c
+++ bonus/tests/ft_list_sort_tests.c
@@ -9,29 +9,29 @@ static void print_list(t_list **begin) {
     }
 }
 
-static void list_sort(t_list **begin_list, int (*cmp)()) {
-    t_list *current;
-    void   *tmp;
-    int swapped;
+// static void list_sort(t_list **begin_list, int (*cmp)()) {
+//     t_list *current;
+//     void   *tmp;
+//     int swapped;
 
-    if (!begin_list || !*begin_list)
-        return;
+//     if (!begin_list || !*begin_list)
+//         return;
 
-    swapped = 1;
-    while (swapped) {
-        swapped = 0;
-        current = *begin_list;
-        while (current->next) {
-            if (cmp(current->data, current->next->data) > 0) {
-                tmp = current->data;
-                current->data = current->next->data;
-                current->next->data = tmp;
-                swapped = 1;
-            }
-            current = current->next;
-        }
-    }
-}
+//     swapped = 1;
+//     while (swapped) {
+//         swapped = 0;
+//         current = *begin_list;
+//         while (current->next) {
+//             if (cmp(current->data, current->next->data) > 0) {
+//                 tmp = current->data;
+//                 current->data = current->next->data;
+//                 current->next->data = tmp;
+//                 swapped = 1;
+//             }
+//             current = current->next;
+//         }
+//     }
+// }
 
 
 static void free_list(t_list **begin) {
@@ -49,40 +49,105 @@ void ft_list_sort_tests() {
     printf("#                ft_list_sort                #\n");
     printf("##############################################\n");
 
-    printf("\n1. Already init list:\n");
+    printf("\n1. Random normal list:\n");
 
-    t_list *list = NULL;
-    ft_list_push_front(&list, "5");
-    ft_list_push_front(&list, "1");
-    ft_list_push_front(&list, "3");
-    ft_list_push_front(&list, "4");
-    ft_list_push_front(&list, "6");
+    t_list *list1 = NULL;
+    ft_list_push_front(&list1, "5");
+    ft_list_push_front(&list1, "1");
+    ft_list_push_front(&list1, "3");
+    ft_list_push_front(&list1, "4");
+    ft_list_push_front(&list1, "6");
 
-    print_list(&list);
+    printf("Before:\n");
+    print_list(&list1);
 
-    list_sort(&list, ft_strcmp);
+    printf("After:\n");
+     ft_list_sort(&list1, ft_strcmp);
+    print_list(&list1);
 
-    printf("--\n");
-    print_list(&list);
+    free_list(&list1);
 
-    free_list(&list);
-    printf("-------\n");
+    printf("\n2. Empty list:\n");
 
-    list = NULL;
-    ft_list_push_front(&list, "5");
-    ft_list_push_front(&list, "1");
-    ft_list_push_front(&list, "3");
-    ft_list_push_front(&list, "4");
-    ft_list_push_front(&list, "6");
+    list1 = NULL;
 
-    print_list(&list);
+    printf("Before:\n");
+    print_list(&list1);
 
-    ft_list_sort(&list, ft_strcmp);
+    printf("After:\n");
+     ft_list_sort(&list1, ft_strcmp);
+    print_list(&list1);
 
-    printf("--\n");
-    print_list(&list);
+    printf("\n3. NULL arg:\n");
 
-    free_list(&list);
+    ft_list_sort(NULL, ft_strcmp);
+
+    printf("\n4. NULL func:\n");
+
+    ft_list_push_front(&list1, "5");
+    ft_list_push_front(&list1, "1");
+    ft_list_push_front(&list1, "3");
+    ft_list_push_front(&list1, "4");
+    ft_list_push_front(&list1, "6");
+
+    printf("Before:\n");
+    print_list(&list1);
+
+    printf("After:\n");
+     ft_list_sort(&list1, NULL);
+    print_list(&list1);
+
+    free_list(&list1);
+
+    printf("\n5. Already sorted list:\n");
+
+    list1 = NULL;
+
+    ft_list_push_front(&list1, "5");
+    ft_list_push_front(&list1, "4");
+    ft_list_push_front(&list1, "3");
+    ft_list_push_front(&list1, "2");
+    ft_list_push_front(&list1, "1");
+
+    printf("Before:\n");
+    print_list(&list1);
+
+    printf("After:\n");
+     ft_list_sort(&list1, ft_strcmp);
+    print_list(&list1);
+
+    free_list(&list1);
+
+    printf("\n6. One node list:\n");
+
+    list1 = NULL;
+
+    ft_list_push_front(&list1, "5");
+
+    printf("Before:\n");
+    print_list(&list1);
+
+    printf("After:\n");
+    ft_list_sort(&list1, ft_strcmp);
+    print_list(&list1);
+
+    free_list(&list1);
+
+    printf("\n6. Two node list:\n");
+
+    list1 = NULL;
+
+    ft_list_push_front(&list1, "4");
+    ft_list_push_front(&list1, "5");
+
+    printf("Before:\n");
+    print_list(&list1);
+
+    printf("After:\n");
+     ft_list_sort(&list1, ft_strcmp);
+    print_list(&list1);
+
+    free_list(&list1);
 
     printf("\n");
 }
